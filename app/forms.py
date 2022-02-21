dfc7341b3034f62d4266ae6fd18894396ed63a55
Riverine-jpg <nateb2001@gmail.com>
2022-02-20 23:30:19 -0500

    form css added

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,15 @@
 import email
 from flask import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired, Email
 
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Full Name", validators=[DataRequired()])
-    email = StringField("Email",validators=[Email(),DataRequired()])
-    subject = StringField("Subject", validators=[DataRequired()])
-    msg = StringField("Message", validators=[DataRequired()])
+    name = StringField("Full Name", validators=[DataRequired()],name = "Full Name")
+    email = StringField("Email",validators=[Email(),DataRequired()],name="Email")
+    subject = StringField("Subject", validators=[DataRequired()],name="Subject")
+    msg = TextAreaField("Message", validators=[DataRequired()],name="Message")
+
 
